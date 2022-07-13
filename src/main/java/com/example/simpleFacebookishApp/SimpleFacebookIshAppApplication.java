c7cb065b61e990372660e00b293cbd7f6f32a368
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-07-13 09:22:24 +0900

    initial commit

---
+++ src/main/java/com/example/simpleFacebookishApp/SimpleFacebookIshAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleFacebookishApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFacebookIshAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFacebookIshAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleFacebookishApp/SimpleFacebookIshAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleFacebookishApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFacebookIshAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
